f39536c5691571bb8ca984eb02668362d8e4d8d8
Skycloudd <51929172+Skycloudd@users.noreply.github.com>
2022-01-18 11:30:47 +0100

    create random_bitflips.py

---
+++ random_bitflips.py
@@ -0,0 +1,29 @@
+import random
+
+
+
+def flip_random(filecontents):
+    bytes_filecontents = bytearray(filecontents)
+    
+    byteindex = random.randint(0, len(bytes_filecontents) - 1)
+    bitindex = random.randint(0, 7)
+    
+    bytes_filecontents[byteindex] ^= 1 << bitindex
+    
+    return bytes(bytes_filecontents)
+
+def main():
+    filename = "test"
+    
+    with open(filename, "rb") as f:
+        filecontents = f.read()
+    
+    filecontents = flip_random(filecontents)
+    
+    print(filecontents)
+    
+    with open(filename, "wb") as f:
+        f.write(filecontents)
+
+if __name__ == "__main__":
+    main()
